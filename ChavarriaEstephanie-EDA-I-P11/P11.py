783e10253061e0c853a1909cec409cdbafabd8b5
Estephanie Chavarria <estefanie.chavarria.mendoza@gmail.com>
2023-05-27 03:13:20 +0000

    main

--- ChavarriaEstephanie-EDA-I-P11/P11.py
+++ ChavarriaEstephanie-EDA-I-P11/P11.py
@@ -1,10 +1,10 @@
-def decimal_a_binario(numero_decimal):
-    if numero_decimal == 0:
+def binario(decimal):
+    if decimal == 0:
         return 0
     else:
-        return (numero_decimal % 2) + 10 * decimal_a_binario(numero_decimal // 2)
+        return (decimal % 2) + 10 * binario(decimal // 2)
 
-def fibonacci_inverso(n):
+def fibonacci(n):
     if n <= 0:
         return []
     elif n == 1:
@@ -12,21 +12,21 @@ def fibonacci_inverso(n):
     elif n == 2:
         return [1, 0]
     else:
-        fib_seq = fibonacci_inverso(n - 1)
-        fib_seq.append(fib_seq[-1] + fib_seq[-2])
-        return fib_seq
+        secuencia = fibonacci(n - 1)
+        secuencia.append(secuencia[-1] + secuencia[-2])
+        return secuencia
 
 opcion = input("Bienvenido, puede realizar las siguientes operaciones:\nSeleccione una opción:\n1) Convertir a binario.\n2) Imprimir serie de Fibonacci.\n3) Salir.\n")
 
 if opcion == "1":
     numero = int(input("Ingrese un número: "))
-    numero_binario = decimal_a_binario(numero)
+    numero_binario = binario(numero)
     print("El numero binario es:", numero_binario)
 elif opcion == "2":
     numero = int(input("Ingrese un número: "))
-    fibonacci_seq = fibonacci_inverso(numero)
+    secuencia = fibonacci(numero)
     print("Los numeros son: ", numero)
-    for num in reversed(fibonacci_seq):
+    for num in reversed(secuencia):
         print(num)
 elif opcion == "3":
     print("El programa ha terminado")
